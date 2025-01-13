eaf181f473f244b74775ee338e055311976657e4
Sharvil Patil <79535235+sharvil1205@users.noreply.github.com>
2025-01-13 16:35:55 +0530

    TODO list (dummy) using *ngFor directive and data binding (#3)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SearchComponent],
+  imports: [RouterOutlet, HeaderComponent, SearchComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

735d7d85cc847481e05583604c60228aae933e71
Sharvil <79535235+sharvil1205@users.noreply.github.com>
2025-01-17 17:45:58 +0530

    Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,37 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SearchComponent, TasksComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SearchComponent,
+    TasksComponent,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })

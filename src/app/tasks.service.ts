1de9a74088a17e61fa0b2428ae691930bbdd5fb9
Sharvil <79535235+sharvil1205@users.noreply.github.com>
2025-01-24 15:25:55 +0530

    Services and API integration

---
+++ src/app/tasks.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TasksService {
+  private readonly API_BASE_URL =
+    'https://freeapi.miniprojectideas.com/api/JWT';
+
+  constructor(private http: HttpClient) {}
+
+  getAllTasks(): Observable<any> {
+    return this.http.get(`${this.API_BASE_URL}/GetAllTaskList`);
+  }
+
+  getTaskById(id: number): Observable<any> {
+    return this.http.get(`${this.API_BASE_URL}/GetTask/${id}`);
+  }
+
+  addTask(task: {
+    itemId: number;
+    taskName: string;
+    taskDescription: string;
+    dueDate: string;
+    isCompleted: boolean;
+    tags: string;
+    completedOn: string;
+    createdOn: string;
+  }): Observable<any> {
+    return this.http.post(`${this.API_BASE_URL}/CreateNewTask`, task);
+  }
+
+  updateTask(task: {
+    itemId: number;
+    taskName: string;
+    taskDescription: string;
+    dueDate: string;
+    isCompleted: boolean;
+    tags: string;
+    completedOn: string;
+  }): Observable<any> {
+    return this.http.put(`${this.API_BASE_URL}/UpdateTask`, task);
+  }
+
+  deleteTask(id: number): Observable<any> {
+    return this.http.delete(`${this.API_BASE_URL}/DeleteTask`, {
+      params: new HttpParams().set('itemId', id),
+    });
+  }
+}
---
+++ src/app/toast.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ToastComponent } from './toast/toast.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToastService {
+  constructor(private snackBar: MatSnackBar) {}
+
+  showSuccess(message: string): void {
+    this.snackBar.openFromComponent(ToastComponent, {
+      data: { message },
+      duration: 3000,
+      panelClass: ['toast-success'],
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+  }
+
+  showError(message: string): void {
+    this.snackBar.openFromComponent(ToastComponent, {
+      data: { message },
+      duration: 3000,
+      panelClass: ['toast-error'],
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+  }
+}

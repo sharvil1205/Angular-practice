5c8834d4824a2802f81412a40bd2385367d07d1b
Sharvil Patil <79535235+sharvil1205@users.noreply.github.com>
2025-01-28 15:07:14 +0530

    Courses layout (#17)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskFormComponent } from './components/tasks/components/task-form/task-form.component';
 import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { MentorComponent } from './components/courses/components/mentor/mentor.component';
+import { MenteeComponent } from './components/courses/components/mentee/mentee.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'allTasks', pathMatch: 'full' },
@@ -17,6 +20,22 @@ export const routes: Routes = [
   {
     path: 'allTasks/edit',
     component: TaskFormComponent,
+  },
+  {
+    path: 'courses',
+    component: CoursesComponent,
+    children: [
+      { path: '', redirectTo: 'mentor', pathMatch: 'full' },
+
+      {
+        path: 'mentor',
+        component: MentorComponent,
+      },
+      {
+        path: 'mentee',
+        component: MenteeComponent,
+      },
+    ],
   },
   { path: '**', component: PageNotFoundComponent },
 ];
---
+++ src/app/components/courses/components/mentee/mentee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mentee',
+  imports: [],
+  templateUrl: './mentee.component.html',
+  styleUrl: './mentee.component.css'
+})
+export class MenteeComponent {
+
+}
